c324824922ff4a308a03e09a66a2cc90c7f90de4
BradG13531 <bradgale6237@gmail.com>
2024-05-26 01:22:35 -0500

    CardManager now contains a reference to the deck.

--- Assets/Scripts/CardManager.cs
+++ Assets/Scripts/CardManager.cs
@@ -4,13 +4,6 @@ using UnityEngine;
 
 public class CardManager : MonoBehaviour
 {
-    public Card card_0;
-    public Card card_1;
-    public Card card_2;
-
-    public Card GetCard(int num)
-    {
-        return null;
-    }
+    public Deck deck;
 
 }
